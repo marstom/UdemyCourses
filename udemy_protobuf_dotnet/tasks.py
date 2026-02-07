56eae2c9438b220b86a179b602e9e277ac3b19f9
Tomasz Marszalek <marstomek@gmail.com>
2026-02-07 14:46:57 +0100

    iclean

--- udemy_protobuf_dotnet/tasks.py
+++ udemy_protobuf_dotnet/tasks.py
@@ -1,9 +1,6 @@
-from invoke import task
-from pathlib import Path
+from invoke import task, Collection
 import os
 
-PROJECT_DIR = Path(__file__).resolve().parent
-## GRPC Regenerate
 
 
 @task
@@ -15,13 +12,9 @@ def source(c):
 def ppath(c):
     c.run("echo $PYTHONPATH")
 
-@task
-def generate_protos(c):
-    c.run("invoke generate-grpc")
-    c.run("invoke generate-grpc-groomadmin")
 
 @task
-def generate_grpc(c):
+def _generate_grpc(c):
     # Generate pb2 + type hints + grpc stubs into proto_output/
     names = [os.path.join("protos", "groom.proto"), os.path.join("protos", "my.proto")]
     for protoc_name in names:
@@ -34,40 +27,33 @@ def generate_grpc(c):
             --grpc_python_out=generated \
             {protoc_name}
             """
-            # f'cd "{PROJECT_DIR}" && '
-            # "python -m grpc_tools.protoc "
-            # "-I protos "
-            # "--python_out=generated "
-            # "--pyi_out=generated "
-            # "--grpc_python_out=generated "
-            # f"{protoc_name}"
         )
 
 
 @task
-def generate_grpc_groomadmin(c):
-    names = [os.path.join("groomadmin", "protos", "groom.proto")]
+def _generate_grpc_groomadmin(c):
+    names = [os.path.join("protos", "groom.proto")]
     folder = os.path.join("groomadmin", "protos")
     folder_base = os.path.join("groomadmin")
     for protoc_name in names:
         c.run(
-            """ 
+            f""" 
             cd groomadmin &&
             python -m grpc_tools.protoc \
             -I ./protos \
             --python_out=generated \
             --pyi_out=generated \
             --grpc_python_out=generated \
-            groom.proto
+            {protoc_name}
             """
-            # "python -m grpc_tools.protoc "
-            # f"-I {folder} "
-            # f"--python_out={folder_base} "
-            # f"--pyi_out={folder_base} "
-            # f"--grpc_python_out={folder_base} "
-            # f"{protoc_name}"
         )
 
+@task(_generate_grpc, _generate_grpc_groomadmin)
+def generate_protos(c):
+    """This generates all protos"""
+    print("Protos generated")
+    # c.run("invoke generate-grpc")
+    # c.run("invoke generate-grpc-groomadmin")
 
 ############# Services
 
@@ -92,3 +78,4 @@ def run_node_client(c):
     # pty - colors
     # c.run("node newsbot/client.js", pty=True, echo=True)
     c.run("node newsbot/client.js")
+
